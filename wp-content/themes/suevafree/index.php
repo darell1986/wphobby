232ebcc64ba36160fa14bf2d0ebc216f50f0c988
Sun XiaoWei <darell1986@gmail.com>
2017-03-14 09:25:09 +0800

    Update index page.

--- wp-content/themes/suevafree/index.php
+++ wp-content/themes/suevafree/index.php
@@ -5,15 +5,10 @@
 	do_action('suevafree_top_sidebar', 'home-top-sidebar-area');
 	do_action('suevafree_header_sidebar', 'home-header-sidebar-area');
 
-	if ( !suevafree_setting('suevafree_home') || suevafree_setting('suevafree_home') == "masonry" ) {
-				
-		get_template_part('layouts/home', 'masonry'); 
 		
-	} else { 
-		
-		get_template_part('layouts/home', 'default'); 
+    get_template_part('layouts/home', 'default');
 			
-	}
+
 
 	do_action('suevafree_full_sidebar', 'full-sidebar-area');
 
