7e9294e20dc1de26a813f420737de8c52dc904d6
Sun XiaoWei <darell1986@gmail.com>
2017-05-05 11:26:13 +0800

    Add top ads code.

--- wp-content/themes/newsmag/header.php
+++ wp-content/themes/newsmag/header.php
@@ -50,7 +50,18 @@ endif;
 	?>
     <header id="masthead" class="site-header" role="banner">
         <div class="site-branding container <?php echo $center_class ? 'centered-branding' : '' ?>">
-            <div class="row">
+            <div class="row"><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+				<!-- WPHobby_Links -->
+				<ins class="adsbygoogle"
+					 style="display:block"
+					 data-ad-client="ca-pub-2524788439684617"
+					 data-ad-slot="3180456681"
+					 data-ad-format="link"></ins>
+				<script>
+					(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
+			<div class="row">
                 <div class="col-md-4 header-logo">
 					<?php
 					$header_textcolor = get_theme_mod( 'header_textcolor' );
